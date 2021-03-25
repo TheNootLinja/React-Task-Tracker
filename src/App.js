0db2d9af524e23f00aea3ffd846b868978024ce2
Nicholas Peters <thenootlinja@gmail.com>
2021-03-24 20:28:57 -0400

    Add code that sends needed reqeusts to the JSON db to perform actions that edit or delete tasks so they reflect the changes in the DB.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import Tasks from './components/Tasks';
@@ -6,41 +6,78 @@ import AddTask from './components/AddTask';
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-])
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+    // Below is what is known as a dependency array, here you would put
+    // anything that you want to trigger useEffect when said thing changes
+  }, [])
+
+  // Fetching Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
+
+  // Fetching Single Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
 
 // Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1;
-  const newTask = {id, ...task};
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json();
+
+  setTasks([...tasks, data])
+
+  // const id = Math.floor(Math.random() * 10000) + 1;
+  // const newTask = {id, ...task};
+  // setTasks([...tasks, newTask]);
+  // setShowAddTask(!showAddTask);
 }
-// Delete Task
-const deleteTask = (id) => {
+// Delete Task From UI and the actual DB/DB file
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
+
   setTasks(tasks.filter(task => task.id !== id))
 }
-// Toggle Reminder
-const toggleReminder = (id) => {
-  console.log(id)
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+// Toggle Reminder Function
+const toggleReminder = async (id) => {
+  // Fetching the task we want to toggle
+  const taskToToggle = await fetchTask(id);
+  // Updating the task and saving it to a variable
+  const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder};
+  // Using a PUT request to replace the task with the updated one with the desired reminder status
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    // These are the headers we need for the PUT request we are doing
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updatedTask)
+  })
+
+  const data = await res.json();
+
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
 }
 // Toggle Showing Add Form
 const showAddForm = () => {
